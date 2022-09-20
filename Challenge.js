c4bacf3eda16380e84be9d3f83f8c7c39a25e780
Sharon Elizabeth Freundlich <elizabeth.freundlich@gmail.com>
2022-09-20 14:28:02 +0300

    Js challenge

---
+++ Challenge.js
@@ -0,0 +1,17 @@
+console.log("Hello world");
+const phoneNumber = +358465888781;
+console.log(phoneNumber);
+if (false) {
+    console.log('phone number ${phoneNumber} is in international format.');
+} else {
+    console.log('phone number ${phoneNumber} is not in International format.')
+}
+
+const capitalizeName = (name) => {
+    console.log(name);
+    const firstLetter = name[0];
+    const rest = name.slice(1); // or .substring, or .substr
+    const capitalizedName = firstLetter.toUpperCase() + rest.toLowerCase();
+    return capitalizedName; 
+}
+console.log(capitalizeName("ezekiel"));
